70362fc82e7e9483ce85ae02f9fe73a64a992a03
Jonathan Undelikwo <jundelikwo@gmail.com>
2023-02-28 22:49:10 +0100

    Finished jest setup

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -9,11 +9,6 @@ import App from '../App';
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
-jest.mock('react-native-keyboard-aware-scroll-view', () => {
-  const KeyboardAwareScrollView = ({children}) => children;
-  return {KeyboardAwareScrollView};
-});
-
 it('renders correctly', () => {
   renderer.create(<App />);
 });
