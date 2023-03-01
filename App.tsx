6eb731d0e78b7793b54d9742fe5664699606a789
Jonathan Undelikwo <jundelikwo@gmail.com>
2023-03-01 08:14:41 +0100

    Added inventory context

--- App.tsx
+++ App.tsx
@@ -9,12 +9,17 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import AuthProvider from 'src/context/auth';
 import Navigation from 'src/navigation/navigation';
+import InventoryProvider from 'src/context/inventory';
+import FlashMessage from 'react-native-flash-message';
 
 function App(): JSX.Element {
   return (
     <NavigationContainer>
       <AuthProvider>
-        <Navigation />
+        <InventoryProvider>
+          <Navigation />
+          <FlashMessage position="top" />
+        </InventoryProvider>
       </AuthProvider>
     </NavigationContainer>
   );
---
+++ src/context/inventory/index.tsx
@@ -0,0 +1,91 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import React, {
+  createContext,
+  useEffect,
+  useState,
+  useContext,
+  useCallback,
+} from 'react';
+import {useAuth} from '../auth';
+
+interface ItemPayloadType {
+  name: string;
+  totalStock: string;
+  price: string;
+  description: string;
+}
+
+interface ItemType extends ItemPayloadType {
+  uuid: string;
+  user: string;
+}
+
+interface UserContextType {
+  items: ItemType[];
+  addItem: (item: ItemPayloadType) => Promise<string>;
+}
+
+const initialContext: UserContextType = {
+  items: [],
+  addItem: () => new Promise(resolve => resolve('Done')),
+};
+
+export const InventoryContext = createContext<UserContextType>(initialContext);
+
+export const InventoryProvider = ({children}: {children: React.ReactNode}) => {
+  const [items, setItems] = useState<ItemType[]>([]);
+  const {user} = useAuth();
+  console.log('items', items);
+
+  useEffect(() => {
+    (async () => {
+      const itemValue = await AsyncStorage.getItem('inventories');
+
+      if (itemValue) {
+        const parsedItems = JSON.parse(itemValue);
+        setItems(parsedItems);
+      }
+    })();
+  }, []);
+
+  const addItem = useCallback(
+    (newItem: ItemPayloadType) => {
+      return new Promise<string>((resolve, reject) => {
+        const formattedItem: ItemType = {
+          ...newItem,
+          uuid: newItem.name.trim().toLowerCase(),
+          user: user?.uuid || '',
+          name: newItem.name.trim(),
+        };
+
+        const foundItem = items.find(
+          item => item.name.toLowerCase() === formattedItem.uuid,
+        );
+
+        if (foundItem) {
+          reject('Name already exist');
+          return;
+        }
+
+        setItems(state => {
+          const newState = [...state, formattedItem];
+
+          AsyncStorage.setItem('user', JSON.stringify(newState));
+          return newState;
+        });
+        resolve('Item added successfully');
+      });
+    },
+    [items, user?.uuid],
+  );
+
+  return (
+    <InventoryContext.Provider value={{items, addItem}}>
+      {children}
+    </InventoryContext.Provider>
+  );
+};
+
+export const useInventory = () => useContext(InventoryContext);
+
+export default InventoryProvider;
